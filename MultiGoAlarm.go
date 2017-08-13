99b5ae5fd726c299ac5ec0e5e7c6a2876a35e5e8
hokorobi <hokorobi.hokorobi@gmail.com>
2017-08-13 21:09:18 +0900

    divide 1

--- MultiGoAlarm.go
+++ MultiGoAlarm.go
@@ -2,25 +2,13 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"os"
-	"regexp"
-	"strconv"
-	"strings"
 	"time"
 
 	"github.com/lxn/walk"
 	. "github.com/lxn/walk/declarative"
 )
-
-type AlarmItem struct {
-	start   *time.Time
-	end     *time.Time
-	message string
-	value   string
-	id      string
-}
 
 type AlarmItems struct {
 	walk.ListModelBase
@@ -30,61 +18,6 @@ type AlarmItems struct {
 func NewAlarmModel() *AlarmItems {
 	m := &AlarmItems{items: make([]AlarmItem, 0)}
 	return m
-}
-
-func (item *AlarmItem) setValue(start time.Time) {
-	hour := "00"
-	minute := "00"
-	second := "00"
-	var index int
-
-	v := item.end.Sub(start).String()
-	index = strings.Index(v, "h")
-	if index > -1 {
-		hour = v[:index]
-		v = v[index+1:]
-	}
-	index = strings.Index(v, "m")
-	if index > -1 {
-		minute = v[:index]
-		v = v[index+1:]
-	}
-	index = strings.Index(v, ".")
-	if index > -1 {
-		second = v[:index]
-	} else {
-		second = v[:strings.Index(v, "s")]
-	}
-	item.value = fmt.Sprintf("%02s:%02s:%02s %s", hour, minute, second, item.message)
-}
-
-func (item *AlarmItem) getTime(s string) (*time.Time, *time.Time) {
-	start := time.Now()
-	// 数字だけなら分として扱う
-	if d, err := time.ParseDuration(s + "m"); err == nil {
-		end := start.Add(d)
-		return &start, &end
-	}
-	// 1h2m などを解釈
-	if d, err := time.ParseDuration(s); err == nil {
-		end := start.Add(d)
-		return &start, &end
-	}
-	// hh:mm
-	re := regexp.MustCompile("^[0-9]+:[0-9]+$")
-	if re.MatchString(s) {
-		hhmm := strings.Split(s, ":")
-		hh, _ := strconv.Atoi(hhmm[0])
-		mm, _ := strconv.Atoi(hhmm[1])
-		end := time.Date(start.Year(), start.Month(), start.Day(), hh, mm, 0, 0, start.Location())
-		// 翌日の hh:mm
-		if start.After(end) {
-			end = end.Add(time.Hour * 24)
-		}
-		return &start, &end
-	}
-
-	return nil, nil
 }
 
 func (items *AlarmItems) add(item AlarmItem) {
@@ -152,32 +85,6 @@ func (m *AlarmItems) ItemCount() int {
 
 func (m *AlarmItems) Value(index int) interface{} {
 	return m.items[index].value
-}
-
-func NewAlarmItem(s string) *AlarmItem {
-	var message string
-	var timeString string
-
-	if strings.Index(s, " ") > 0 {
-		timeString = s[0:strings.Index(s, " ")]
-		message = s[strings.Index(s, " "):]
-	} else {
-		timeString = s
-		message = ""
-	}
-
-	item := new(AlarmItem)
-	start, end := item.getTime(timeString)
-	if start == nil {
-		return nil
-	}
-	item.start = start
-	item.end = end
-	item.message = message
-	item.setValue(*start)
-	item.id = fmt.Sprint(start.Unix())
-
-	return item
 }
 
 type MyMainWindow struct {
---
+++ alarmItem.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type AlarmItem struct {
+	start   *time.Time
+	end     *time.Time
+	message string
+	value   string
+	id      string
+}
+
+func (item *AlarmItem) setValue(start time.Time) {
+	hour := "00"
+	minute := "00"
+	second := "00"
+	var index int
+
+	v := item.end.Sub(start).String()
+	index = strings.Index(v, "h")
+	if index > -1 {
+		hour = v[:index]
+		v = v[index+1:]
+	}
+	index = strings.Index(v, "m")
+	if index > -1 {
+		minute = v[:index]
+		v = v[index+1:]
+	}
+	index = strings.Index(v, ".")
+	if index > -1 {
+		second = v[:index]
+	} else {
+		second = v[:strings.Index(v, "s")]
+	}
+	item.value = fmt.Sprintf("%02s:%02s:%02s %s", hour, minute, second, item.message)
+}
+
+func (item *AlarmItem) getTime(s string) (*time.Time, *time.Time) {
+	start := time.Now()
+	// 数字だけなら分として扱う
+	if d, err := time.ParseDuration(s + "m"); err == nil {
+		end := start.Add(d)
+		return &start, &end
+	}
+	// 1h2m などを解釈
+	if d, err := time.ParseDuration(s); err == nil {
+		end := start.Add(d)
+		return &start, &end
+	}
+	// hh:mm
+	re := regexp.MustCompile("^[0-9]+:[0-9]+$")
+	if re.MatchString(s) {
+		hhmm := strings.Split(s, ":")
+		hh, _ := strconv.Atoi(hhmm[0])
+		mm, _ := strconv.Atoi(hhmm[1])
+		end := time.Date(start.Year(), start.Month(), start.Day(), hh, mm, 0, 0, start.Location())
+		// 翌日の hh:mm
+		if start.After(end) {
+			end = end.Add(time.Hour * 24)
+		}
+		return &start, &end
+	}
+
+	return nil, nil
+}
+
+func NewAlarmItem(s string) *AlarmItem {
+	var message string
+	var timeString string
+
+	if strings.Index(s, " ") > 0 {
+		timeString = s[0:strings.Index(s, " ")]
+		message = s[strings.Index(s, " "):]
+	} else {
+		timeString = s
+		message = ""
+	}
+
+	item := new(AlarmItem)
+	start, end := item.getTime(timeString)
+	if start == nil {
+		return nil
+	}
+	item.start = start
+	item.end = end
+	item.message = message
+	item.setValue(*start)
+	item.id = fmt.Sprint(start.Unix())
+
+	return item
+}

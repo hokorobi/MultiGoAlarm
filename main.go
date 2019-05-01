c0d18d53a7f403dfb14d2151365224164aaf3747
hokorobi <hokorobi.hokorobi@gmail.com>
2019-05-01 19:31:56 +0900

    divide notifyIcon

--- main.go
+++ main.go
@@ -47,7 +47,7 @@ func main() {
 		// t.Stop()
 	}()
 
-	ni := notifyIcon(app.mw)
+	ni := NotifyIcon(app.mw)
 	defer ni.Dispose()
 
 	if _, err := (declarative.MainWindow{
@@ -126,37 +126,4 @@ func (app *App) update() []AlarmItem {
 
 func (app *App) clickQuit() {
 	os.Exit(0)
-}
-
-func notifyIcon(mw *walk.MainWindow) *walk.NotifyIcon {
-	// load icon
-	icon, err := walk.NewIconFromFile("MultiGoAlarm.ico")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	ni, err := walk.NewNotifyIcon(mw)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Set the icon and a tool tip text.
-	if err := ni.SetIcon(icon); err != nil {
-		log.Fatal(err)
-	}
-
-	// We put an exit action into the context menu.
-	exitAction := walk.NewAction()
-	if err := exitAction.SetText("E&xit"); err != nil {
-		log.Fatal(err)
-	}
-	exitAction.Triggered().Attach(func() { walk.App().Exit(0) })
-	if err := ni.ContextMenu().Actions().Add(exitAction); err != nil {
-		log.Fatal(err)
-	}
-	// The notify icon is hidden initially, so we have to make it visible.
-	if err := ni.SetVisible(true); err != nil {
-		log.Fatal(err)
-	}
-	return ni
 }

cbdca001f97f694b19c6c7f3cf0b3ea79ed74d13
hokorobi <hokorobi.hokorobi@gmail.com>
2020-04-29 16:05:21 +0900

    Done move window

--- alarmWindow.go
+++ alarmWindow.go
@@ -1,31 +1,13 @@
 package main
 
 import (
+	"context"
 	"time"
 
 	"github.com/lxn/walk"
 	"github.com/lxn/walk/declarative"
 	"github.com/lxn/win"
 )
-
-type aw struct {
-	mw *walk.MainWindow
-}
-
-func (aw *aw) update() {
-
-}
-func newAw() aw {
-	var aw aw
-	var err error
-
-	aw.mw, err = walk.NewMainWindow()
-	if err != nil {
-		Logf(err)
-	}
-
-	return aw
-}
 
 // AlarmWindow はアラームウィンドウを表示する関数
 func AlarmWindow(s string) {
@@ -39,20 +21,7 @@ func AlarmWindow(s string) {
 		message = s
 	}
 
-	t := time.NewTicker(time.Second)
-	defer t.Stop()
-	go func() {
-		for {
-			select {
-			case <-t.C:
-				aw.update()
-			}
-		}
-	}()
-
 	winsize := declarative.Size{Width: 300, Height: 300}
-	// TODO: ウィンドウを動かして目立たせる
-	// "user interface - How to set window position and make it unresizable in Go walk - Stack Overflow" https://stackoverflow.com/questions/25949966/how-to-set-window-position-and-make-it-unresizable-in-go-walk
 	// FIXME: too big button
 	// FIXME: too small font
 	err := declarative.MainWindow{
@@ -86,5 +55,73 @@ func AlarmWindow(s string) {
 	// "ウインドウサイズ" http://eternalwindows.jp/winbase/window/window13.html
 	win.SetWindowPos(aw.mw.Handle(), win.HWND_TOPMOST, 0, 0, 0, 0, win.SWP_FRAMECHANGED|win.SWP_NOMOVE|win.SWP_NOSIZE)
 
+	aw.alarm()
+
 	aw.mw.Run()
+}
+
+type aw struct {
+	mw   *walk.MainWindow
+	orgX int32
+	orgY int32
+}
+
+func (aw *aw) alarm() {
+	aw.orgX, aw.orgY = aw.getWindowPos()
+	// "Golangで周期的に実行するときのパターン - Qiita" https://qiita.com/tetsu_koba/items/1599408f537cb513b250
+	ctx, cancel := context.WithCancel(context.Background())
+	t := time.NewTicker(100 * time.Millisecond)
+	go func(ctx context.Context) {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-t.C:
+				aw.cycleMove()
+			}
+		}
+	}(ctx)
+	go func() {
+		time.Sleep(time.Second)
+		t.Stop()
+		cancel()
+	}()
+}
+func (aw *aw) cycleMove() {
+	curX, curY := aw.getWindowPos()
+	if aw.orgX == curX {
+		if aw.orgY == curY {
+			aw.moveWindow(aw.orgX-50, aw.orgY)
+		} else if aw.orgY+50 == curY {
+			aw.moveWindow(aw.orgX, aw.orgY-50)
+		} else if aw.orgY-50 == curY {
+			aw.moveWindow(aw.orgX+50, aw.orgY)
+		} else {
+			aw.moveWindow(aw.orgX, aw.orgY)
+		}
+	} else if aw.orgX+50 == curX {
+		aw.moveWindow(aw.orgX, aw.orgY)
+	} else if aw.orgX-50 == curX {
+		aw.moveWindow(aw.orgX, aw.orgY-50)
+	}
+}
+func (aw *aw) moveWindow(x int32, y int32) {
+	win.SetWindowPos(aw.mw.Handle(), win.HWND_TOPMOST, x, y, 0, 0, win.SWP_FRAMECHANGED|win.SWP_NOSIZE)
+}
+func (aw *aw) getWindowPos() (int32, int32) {
+	// https://github.com/lxn/walk/blob/55ccb3a9f5c1dae7b1c94f70ea4f9db6afcb5021/form.go#L598-L615
+	var r win.RECT
+	win.GetWindowRect(aw.mw.Handle(), &r)
+	return r.Left, r.Top
+}
+func newAw() aw {
+	var aw aw
+	var err error
+
+	aw.mw, err = walk.NewMainWindow()
+	if err != nil {
+		Logf(err)
+	}
+
+	return aw
 }

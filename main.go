20f9c5176a3df38def2d1149612526fa661d30b6
hokorobi <hokorobi.hokorobi@gmail.com>
2020-05-04 14:49:06 +0900

    Improve window management

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"github.com/go-toast/toast"
 	"github.com/lxn/walk"
 	"github.com/lxn/walk/declarative"
+	"github.com/lxn/win"
 	"github.com/rodolfoag/gow32"
 )
 
@@ -43,8 +44,8 @@ func main() {
 		}
 	}()
 
-	app.ni = newNotifyIcon(&app)
-	defer app.ni.Dispose()
+	logg("Run.")
+	defer logg("Stop.")
 
 	if len(os.Args) > 1 {
 		item := newAlarmItem(strings.Join(os.Args[1:], " "))
@@ -56,23 +57,47 @@ func main() {
 		}
 	}
 
-	logg("Run.")
-	defer logg("Stop.")
-
-	if _, err := (declarative.MainWindow{
+	err = declarative.MainWindow{
 		AssignTo: &app.mw,
 		Title:    "MultiGoAlarm",
+		MinSize:  declarative.Size{Width: 400, Height: 300},
+		Size:     declarative.Size{Width: 400, Height: 300},
 		Visible:  false,
-	}.Run()); err != nil {
+		Layout:   declarative.VBox{},
+		Children: []declarative.Widget{
+			declarative.ListBox{
+				AssignTo:        &app.lb,
+				Model:           app.list,
+				OnItemActivated: app.lbItemActivated,
+				Row:             10,
+			},
+			declarative.PushButton{
+				Text:      "&Add",
+				OnClicked: app.clickAddDlg,
+			},
+		},
+		// Minimize to hide
+		OnSizeChanged: func() {
+			if win.IsIconic(app.mw.Handle()) {
+				app.mw.Hide()
+			}
+		},
+	}.Create()
+	if err != nil {
 		logf(err)
 	}
+
+	// https://github.com/lxn/walk/issues/127
+	app.addNotifyIcon()
+	app.mw.Run()
 }
 
-// App はこのアプリ全体の型
 type app struct {
-	mw   *walk.MainWindow
-	list *alarmList
-	ni   *walk.NotifyIcon
+	mw    *walk.MainWindow
+	lb    *walk.ListBox
+	list  *alarmList
+	ni    *walk.NotifyIcon
+	count int
 }
 
 func newApp() app {
@@ -88,6 +113,22 @@ func newApp() app {
 func (app *app) update() {
 	items := app.list.update()
 	app.alarm(items)
+
+	if !app.mw.Visible() {
+		return
+	}
+
+	if app.count == 0 && app.count == len(app.list.list) {
+		return
+	}
+
+	idx := app.lb.CurrentIndex()
+	app.lb.SetModel(app.list)
+	app.count = len(app.list.list)
+	err := app.lb.SetCurrentIndex(idx)
+	if err != nil {
+		logg(err)
+	}
 }
 func (app *app) alarm(items []alarmItem) {
 	for i := range items {
@@ -95,6 +136,86 @@ func (app *app) alarm(items []alarmItem) {
 		logg("Alarm: " + items[i].End.Format("15:04:05") + " " + items[i].Message)
 		time.Sleep(100 * time.Millisecond)
 	}
+}
+func (app *app) lbItemActivated() {
+	if app.lb.CurrentIndex() < 0 {
+		return
+	}
+
+	app.list.del(app.lb.CurrentIndex())
+	app.lb.SetModel(app.list)
+}
+func (app *app) clickAddDlg() {
+	newText := new(additionalAlarmText)
+	cmd, err := additionalDialog(app.mw, newText)
+	if err != nil {
+		walk.MsgBox(app.mw, "Error", "Enter valid time", walk.MsgBoxOK|walk.MsgBoxIconError)
+		return
+	}
+	if cmd != walk.DlgCmdOK {
+		return
+	}
+
+	item := newAlarmItem(newText.Text)
+	if item == nil {
+		walk.MsgBox(app.mw, "Error", "Enter valid time", walk.MsgBoxOK|walk.MsgBoxIconError)
+		return
+	}
+	app.list.add(*item)
+	notification(*item)
+	// app.lb.SetModel は app.update() で反映
+}
+func (app *app) addNotifyIcon() {
+	var err error
+	app.ni, err = walk.NewNotifyIcon(app.mw)
+	if err != nil {
+		logf(err)
+	}
+
+	// FIXME: Make a clear icon
+	icon, err := walk.Resources.Icon("alarm-check.ico")
+	if err != nil {
+		logg(err)
+	}
+	err = app.mw.SetIcon(icon)
+	if err != nil {
+		logg(err)
+	}
+	err = app.ni.SetIcon(icon)
+	if err != nil {
+		logg(err)
+	}
+
+	// We put an exit action into the context menu.
+	exitAction := walk.NewAction()
+	err = exitAction.SetText("E&xit")
+	if err != nil {
+		logf(err)
+	}
+	exitAction.Triggered().Attach(
+		func() {
+			app.ni.Dispose()
+			// TODO: Improve exit
+			os.Exit(0)
+		},
+	)
+	err = app.ni.ContextMenu().Actions().Add(exitAction)
+	if err != nil {
+		logf(err)
+	}
+
+	app.ni.SetVisible(true)
+	app.ni.MouseDown().Attach(func(x, y int, button walk.MouseButton) {
+		if button == walk.LeftButton {
+			app.mw.Show()
+			win.ShowWindow(app.mw.Handle(), win.SW_RESTORE)
+		}
+	})
+
+}
+
+type additionalAlarmText struct {
+	Text string
 }
 
 func notification(item alarmItem) {
